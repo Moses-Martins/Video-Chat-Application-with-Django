f1efb581a96329f4d3a15b0bf729de357275fd5d
Moses Martins <mmoses061@gmail.com>
2026-01-20 17:44:01 +0100

    Update allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4rw+91@u55yrg0r^0&&7hjsa_0of%8rn_xn&meili!st@+6^a6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-server-docker.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
